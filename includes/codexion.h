f36d1d12d253a4af08215973c92e9081d350f1ad
thasampa <thallessampaio8@gmail.com>
2026-07-31 14:12:04 -0300

    fix: fixing norm errors

--- includes/codexion.h
+++ includes/codexion.h
@@ -6,7 +6,7 @@
 /*   By: thasampa <thasampa@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/24 16:44:08 by thasampa          #+#    #+#             */
-/*   Updated: 2026/07/24 16:44:09 by thasampa         ###   ########.fr       */
+/*   Updated: 2026/07/31 14:11:30 by thasampa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,24 +100,24 @@ long long	get_time_ms(void);
 void		log_state(t_coder *coder, const char *state);
 
 /* Initialization */
-int		init_engine(t_engine *engine, t_config config);
-int		init_dongles(t_engine *engine);
-void	init_coders(t_engine *engine);
+int			init_engine(t_engine *engine, t_config config);
+int			init_dongles(t_engine *engine);
+void		init_coders(t_engine *engine);
 
 /* Cleanup */
-void	cleanup_engine(t_engine *engine);
+void		cleanup_engine(t_engine *engine);
 
 /* Heap */
-t_heap	*heap_create(int capacity);
-void	heap_destroy(t_heap *heap);
-int		heap_push(t_heap *heap, t_request request, t_sched sched);
-int		heap_peek(t_heap *heap, t_request *request);
-int		heap_pop(t_heap *heap, t_sched sched, t_request *request);
-int		request_has_priority(t_request a, t_request b, t_sched sched);
-void	swap_requests(t_request *a, t_request *b);
+void		heap_destroy(t_heap *heap);
+int			heap_push(t_heap *heap, t_request request, t_sched sched);
+int			heap_peek(t_heap *heap, t_request *request);
+int			heap_pop(t_heap *heap, t_sched sched, t_request *request);
+int			request_has_priority(t_request a, t_request b, t_sched sched);
+void		swap_requests(t_request *a, t_request *b);
+t_heap		*heap_create(int capacity);
 
 /* Simulation */
-int		run_simulation(t_engine *engine);
-void	*coder_routine(void *arg);
+int			run_simulation(t_engine *engine);
+void		*coder_routine(void *arg);
 
 #endif
